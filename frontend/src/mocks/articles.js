f496d347b393a3c21e03b245e787fdebe967c3ae
Markos <markosmk@me.com>
2021-11-09 00:48:45 -0300

    [api] false data mock

---
+++ frontend/src/mocks/articles.js
@@ -0,0 +1,39 @@
+const articles = [
+  {
+    title: 'Learn React for all world',
+    slug: 'learn-react',
+    category: 'javascript',
+    image: 'https://noonpost.netlify.app/html/template/assets/img/blog/6.jpg',
+    content:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod tenetur quis, voluptatum dicta suscipit fuga cum perspiciatis assumenda dolorum error minima adipisci molestias unde amet accusantium inventore in explicabo voluptatem. amet consectetur adipisicing elit. Quod tenetur quis, voluptatum dicta suscipit fuga cum perspiciatis assumenda dolorum error minima adipisci molestias unde amet accusantium inventore in explicabo voluptatem. amet consectetur adipisicing elit. Quod tenetur quis, voluptatum dicta suscipit fuga cum perspiciatis assumenda dolorum error minima adipisci molestias unde amet accusantium inventore in explicabo voluptatem.',
+  },
+  {
+    title: 'Learn NextJs, it is brother of React',
+    slug: 'learn-nextjs',
+    category: 'javascript',
+    image:
+      'https://awcdn1.ahmad.works/writing/wp-content/uploads/2015/05/yellow-and-gray-industrial-office-PFDQ5CR-1-267x205.jpg',
+    content:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod tenetur quis, voluptatum dicta suscipit fuga cum perspiciatis assumenda dolorum error minima adipisci molestias unde amet accusantium inventore in explicabo voluptatem.',
+  },
+  {
+    title: 'Stylish Kitchen And Dining Room With Functional Ideas',
+    slug: 'learn-nextjs-4',
+    category: 'javascript',
+    image:
+      'https://awcdn1.ahmad.works/writing/wp-content/uploads/2015/05/kitchen-and-dining-room-P5JHHM6-267x205.jpg',
+    content:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod tenetur quis, voluptatum dicta suscipit fuga cum perspiciatis assumenda dolorum error minima adipisci molestias unde amet accusantium inventore in explicabo voluptatem.',
+  },
+  {
+    title: 'My Adventure in Alps, One of The Heighest Mountains!',
+    slug: 'learn-nextjs-kaka',
+    category: 'javascript',
+    image:
+      'https://awcdn1.ahmad.works/writing/wp-content/uploads/2015/05/Blue-Enterior-Design-267x205.jpg',
+    content:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod tenetur quis, voluptatum dicta suscipit fuga cum perspiciatis assumenda dolorum error minima adipisci molestias unde amet accusantium inventore in explicabo voluptatem.',
+  },
+];
+
+export default articles;
